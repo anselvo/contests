32adc6b0c2837b75622b55a2b42fb1c99db6a027
Andrei Sergeev <aelsergeev@gmail.com>
2023-09-21 20:45:10 +0300

    add usage of lib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use playground::{algorithms, structures};
+
 fn main() {
     println!("Hello Playground!");
+    println!("{:?}", structures::vector::Vec::new());
+    println!("{:?}", algorithms::search::binary_while(vec!(1, 2, 3, 4), 3).unwrap());
 }
